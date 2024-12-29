413077aec6e2b76dc355e64e4d9f8b76635b9fa3
amirhabibzadeh <amirhabibzadeh1350@gmail.com>
2024-12-29 18:27:35 +0330

    initiate git for rabbit sample

---
+++ RabbitSample/Service/OrderPushEngineServices.cs
@@ -0,0 +1,26 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+namespace RabbitSample.Service
+{
+    public static class OrderPushEngineServices
+    {
+        public static async Task<bool> PushMessage(string message)
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost",
+                UserName = "guest",
+                Password = "guest"
+            };
+
+            var connection = await factory.CreateConnectionAsync();
+            var channel = await connection.CreateChannelAsync();
+
+            await channel.QueueDeclareAsync(queue: "orders", durable: true, exclusive: false, autoDelete: false, arguments: null);
+            var body = Encoding.UTF8.GetBytes(message);
+            await channel.BasicPublishAsync(exchange:"",routingKey:"orders",body);
+            return true;
+        }
+    }
+}
